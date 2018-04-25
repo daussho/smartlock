b24d88a385fd0f755b7c8188c43cf58e31610c3e
Abdul Hafizh Firdaus <abdulhafizh9@gmail.com>
2018-04-26 01:02:35 +0700

    fix home and session

--- smartlock/urls.py
+++ smartlock/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('login', views.login, name='login'),
     path('home', views.home, name='home'),
+    path('logout', views.logout, name='logout'),
     #path('date', views.date, name='login'),
 
 ]
--- smartlock/views.py
+++ smartlock/views.py
@@ -8,6 +8,7 @@ from django.shortcuts import redirect
 from django.db import connections
 
 def index(request):
+    request.session['status'] = 'guest'
     return render(request, 'login.html', {})
 
 def login(request):
@@ -19,31 +20,48 @@ def login(request):
         pwd = (request.POST['pwd']).encode()
         user.password = hashlib.sha256(pwd).hexdigest()
 
-        cek = len(Users.objects.filter(user_id = user.user_id))
+        cek = len(Users.objects.filter(user_id = user.user_id, password = user.password))
 
         if ( cek == 1):
             template = loader.get_template('tes.html')
             context = {
                 'user_id' : user.user_id,
                 'password' : user.password,
-                'tes' : tes
+                #'tes' : tes
             }
 
             hasil = Users.objects.all()
 
-            return HttpResponse(template.render(context, request))
-        else:
+            #return HttpResponse(template.render(context, request))
+            request.session['status'] = 'user'
+            request.session['user_name'] = user.user_name
+            request.session['user_id'] = user.user_id
             return redirect('home')
+        else:
+            return redirect('index')
     else:
         return redirect('index')
 
 def home(request):
     #current_date = datetime.datetime.now()
+    if (request.session['status'] == 'guest'):
+        return redirect('index')
     template = loader.get_template('home.html')
     context = {
         #'current_date': current_date,
     }
     return HttpResponse(template.render(context, request))
+
+def logout(request):
+    #current_date = datetime.datetime.now()
+    template = loader.get_template('home.html')
+    context = {
+        #'current_date': current_date,
+    }
+    #return HttpResponse(template.render(context, request))
+    request.session.flush()
+    request.session['status'] = 'guest'
+    return redirect('index')
 
 def tes(request):
     current_date = datetime.datetime.now()
